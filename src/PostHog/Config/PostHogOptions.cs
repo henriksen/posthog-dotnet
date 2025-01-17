8bb695ce32930e73e00022245655ce4ddfd334b7
Phil Haack <haacked@gmail.com>
2025-01-17 14:19:37 -0800

    Add a `PersonalApiKey` setting
    And flesh out how to set these.

--- src/PostHog/Config/PostHogOptions.cs
+++ src/PostHog/Config/PostHogOptions.cs
@@ -6,9 +6,26 @@ namespace PostHog.Config;
 public class PostHogOptions : AsyncBatchHandlerOptions
 {
     /// <summary>
-    /// The project API key.
+    /// The project API key that identifies which project this client works with.
     /// </summary>
+    /// <remarks>
+    /// You can find this https://us.posthog.com/project/{YOUR_PROJECT_ID}/settings/project
+    /// </remarks>
     public string? ProjectApiKey { get; set; }
+
+    /// <summary>
+    /// Optional personal API key for local feature flag evaluation.
+    /// </summary>
+    /// <remarks>
+    ///
+    /// You can find this https://us.posthog.com/project/{YOUR_PROJECT_ID}/settings/user-api-keys
+    /// When developing an ASP.NET Core project locally, we recommend setting this in your user secrets.
+    /// <c>
+    /// dotnet user-secrets --project your/project/path.csproj set PostHog:PersonalApiKey YOUR_PERSONAL_API_KEY
+    /// </c>
+    /// In other cases, use an appropriate secrets manager, configuration provider, or environment variable.
+    /// </remarks>
+    public string? PersonalApiKey { get; set; }
 
     /// <summary>
     /// PostHog API host, usually 'https://us.i.posthog.com' (default) or 'https://eu.i.posthog.com'
